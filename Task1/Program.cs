5cf80608e6ff433966a316352a42f5b77f41db65
Nikolaev1ch <vsharanov@yandex.ru>
2024-01-22 16:53:06 +0500

    add some varible Lesson1:Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Hello world");
+
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
