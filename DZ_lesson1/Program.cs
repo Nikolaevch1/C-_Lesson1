ec3339246a28229c455b1f00d746e748511597d6
Nikolaev1ch <vsharanov@yandex.ru>
2024-01-22 18:13:09 +0500

    add lesson1: task3

---
+++ DZ_lesson1/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = 8;
+int b = 4;
+int c = 5;
+int d = 3;
+int sum = (a * b) / (c + d);
+Console.WriteLine(sum);
---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 5;
+int b = 7;
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
